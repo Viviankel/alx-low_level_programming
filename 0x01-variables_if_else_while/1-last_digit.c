f9fa112839e409b49ba9810efb9dc38b42771662
Viviankel <vivian.kellyyy@gmail.com>
2022-07-04 10:48:42 -0700

    Task 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,28 +1,24 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
+#include <time.h>
 /**
-* main - Determine values
-*
-* Return: Always 0 (Success)
-*/
+    * main - Program entry point
+    *   * Return: 0 on success. Error code otherwise
+    *    **/
 int main(void)
 {
+       	int n, lastDigit;
 
-int n, l;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-l = n % 10;
-if (l > 5)
-{
-printf("Last digit of %d is %d and is greater than 5\n", n, l);
-}
-else if (l == 0)
-{										printf("Last digit of %d is %d and is 0\n", n, l);
-}	
-else										{										printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-}
-return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastDigit= n % 10;
+	if(lastDigit > 5)				
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	else if (lastDigit== 0)				
+		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	else if (lastDigit < 6 && lastDigit != 0)
+	{	printf("Last digit of %d is %d and is less than 6 and ", n, lastDigit);
+		printf("not 0\n");
+	}								
+	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char a;
+for (a = "a"; a <= "z" ; a ==)
+putchar(a);
+putchar("\n");
+return (0);
