25478e6c2483b0c349d5f9e8fea5eb3c4a8659d0
Viviankel <vivian.kellyyy@gmail.com>
2022-07-05 10:38:29 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
-* print_alphabet - Prints the alphabets, in lowercase, followed by a new line
-*
-* Return: Always 0 (Success)
-*/
+ *  * print_alphabet - prints the alphabet in lowercase followed by a new line.
+ *    **/
 void print_alphabet(void)
 {
 	char ch;
