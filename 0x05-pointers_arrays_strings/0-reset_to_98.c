6cc7faf213a698425a3e917165f93fc30e04832e
Viviankel <vivian.kellyyy@gmail.com>
2022-07-12 04:03:56 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * * reset_to_98 - takes pointer to an int as a parameter and updates it
+ * * @n: pointer to an int
+ * **/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
